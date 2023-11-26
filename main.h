6a5057d863dc6ebbd414594e7bae06ef670a2e4c
CreatorCosmo <girizarrymartell14@gmail.com>
2023-11-25 23:42:30 -0400

    added man_3_printf

--- main.h
+++ main.h
@@ -12,41 +12,40 @@
 #define F_PLUS 2
 #define F_ZERO 4
 #define F_HASH 8
-#define G_SPACE 16
+#define F_SPACE 16
 
 /* SIZES */
 #define S_LONG 2
 #define S_SHORT 1
 
 /**
-* struct form - Struct op
-* LOMAX
-* @form: Format.
-* @funct: Function associated.
-*/
-
-struct form
+ * struct fmt - Struct op
+ *
+ * @fmt: The format.
+ * @fn: The function associated.
+ */
+struct fmt
 {
-	char form;
-	int (*funct)(va_list, char[], int, int, int, int);
+	char fmt;
+	int (*fn)(va_list, char[], int, int, int, int);
 };
 
-/**
-* typedef struct form form_t - Struct op
-*
-* @form: The format.
-* @form_t: Function associated.
-*/
 
-typedef struct form form_t;
+/**
+ * typedef struct fmt fmt_t - Struct op
+ *
+ * @fmt: The format.
+ * @fm_t: The function associated.
+ */
+typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
-int handle_print(const char *form, int *i,
+int handle_print(const char *fmt, int *i,
 va_list list, char buffer[], int flags, int width, int precision, int size);
 
-/** Functions **/
+/****************** FUNCTIONS ******************/
 
-/* Functions to print chars and strings */
+/* Funtions to print chars and strings */
 int print_char(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 int print_string(va_list types, char buffer[],
@@ -107,12 +106,5 @@ int write_unsgnd(int is_negative, int ind,
 char buffer[],
 	int flags, int width, int precision, int size);
 
-/* UTILS */
-int is_printable(char);
-int append_hexa_code(char, char[], int);
-int is_digit(char);
-
-long int convert_size_number(long int num, int size);
-long int convert_size_unsgnd(unsigned long int num, int size);
-
 #endif /* MAIN_H */
+
